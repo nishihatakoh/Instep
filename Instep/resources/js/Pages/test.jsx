ac58141fdd73a7a42a5a1b4fa270b7d01a891192
nishihatakoh <koh.s13.kk@gmail.com>
2024-04-29 22:44:40 +0900

    first commit

---
+++ Instep/resources/js/Pages/test.jsx
@@ -0,0 +1,8 @@
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head } from '@inertiajs/react';
+
+export default function Dashboard() {
+    return (
+        <h1>test</h1>
+    );
+}
